aa5def08f197ca93222737b3fec5855150449eed
JulienBe <bertozzijulien@gmail.com>
2017-11-19 20:07:58 +0100

    handle just pressed and key up

--- src/be/julien/seed/InputHub.kt
+++ src/be/julien/seed/InputHub.kt
@@ -4,20 +4,36 @@ class InputHub {
 
     private val keyUpMapping: MutableMap<Int, () -> Unit> = mutableMapOf()
     private val keyPressedMapping: MutableMap<Int, () -> Unit> = mutableMapOf()
+    private val keyJustPressedMapping: MutableMap<Int, () -> Unit> = mutableMapOf()
     private var click: (() -> Unit)? = null
+    private val keyPressedCurrent = mutableSetOf<Int>()
+    private val keyPressedOld = mutableSetOf<Int>()
 
-    fun keyUp(keycode: Int): Boolean {
-        checkKey(keycode, keyUpMapping)
-        return true
+    fun act(inputHandler: InputHandler) {
+        keyPressedCurrent.clear()
+
+        checkKeys(inputHandler)
+
+        keyPressedOld.clear()
+        keyPressedOld.addAll(keyPressedCurrent)
     }
 
-    fun act() {
-//        keyPressedMapping.forEach { key, value ->
-//            if (Gdx.input.isKeyPressed(key))
-//                value.invoke()
-//        }
-//        if (Gdx.input.isTouched)
-//            click?.invoke()
+    private fun checkKeys(inputHandler: InputHandler) {
+        keyPressedMapping.forEach { key, value ->
+            if (inputHandler.isKeyPressed(key)) {
+                keyPressedCurrent.add(key)
+                value.invoke()
+            }
+        }
+        keyJustPressedMapping.forEach { key, value ->
+            if (inputHandler.isKeyJustPressed(key)) {
+                value.invoke()
+                keyPressedCurrent.add(key)
+            }
+        }
+        if (inputHandler.isTouched())
+            click?.invoke()
+        keyPressedOld.minus(keyPressedCurrent).forEach { key: Int -> keyUpMapping[key]?.invoke() }
     }
 
     fun addKeyUp(key: Int, function: () -> Unit) {
@@ -26,16 +42,17 @@ class InputHub {
     fun addKeyPressed(key: Int, function: () -> Unit) {
         keyPressedMapping.put(key, function)
     }
-
-    fun checkKey(keycode: Int, mapping: MutableMap<Int, () -> Unit>) {
-        mapping.forEach {
-            key, value ->
-            if (keycode == key)
-                value.invoke()
-        }
+    fun addKeyJustPressed(key: Int, function: () -> Unit) {
+        keyJustPressedMapping.put(key, function)
     }
-
     fun addClick(function: () -> Unit) {
         click = function
     }
+
+}
+
+interface InputHandler {
+    fun isKeyJustPressed(keycode: Int): Boolean
+    fun isKeyPressed(keycode: Int): Boolean
+    fun isTouched(): Boolean
 }
